091e431315455a8c7c85f8ff4d0376c46913dd6a
yytandrea <andrea.y@wustl.edu>
2025-04-02 13:32:22 -0500

    8-first square click gets X

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ import './App.css'
 }*/
 
 
-function Square( {value}){
+function Square( {value, onSquareClick}){
   //return <button className="square">{value}</button>
   /*const [value, setValue] = useState(null);
   function handleClick() {
@@ -51,29 +51,40 @@ function Square( {value}){
       {value}
     </button>
   );*/
-  return <button className="square">{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 
 }
 
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  
+  function handleClick() {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "X";
+    setSquares(nextSquares);
+  }
+
   return(
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={handleClick}/>
+        <Square value={squares[4]} onSquareClick={handleClick}/>
+        <Square value={squares[5]} onSquareClick={handleClick}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={handleClick}/>
+        <Square value={squares[7]} onSquareClick={handleClick}/>
+        <Square value={squares[8]} onSquareClick={handleClick}/>
       </div>
     </>
   );
