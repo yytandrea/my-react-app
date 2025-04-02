a57970550305cdded850b127ce4568742e6ec714
yytandrea <andrea.y@wustl.edu>
2025-04-02 13:57:45 -0500

    16- added states to Game to track next player and history of moves

--- src/App.jsx
+++ src/App.jsx
@@ -132,6 +132,10 @@ function calculateWinner(squares) {
 
 
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
   return (
     <div className="game">
       <div className="game-board">
