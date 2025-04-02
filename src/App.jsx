ea5d4298f6d5e769e1843aad3fce1eb48114e1f4
yytandrea <andrea.y@wustl.edu>
2025-04-02 12:56:25 -0500

    7-store board in array

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,9 @@ import './App.css'
 }*/
 
 
-function Square(){
+function Square( {value}){
   //return <button className="square">{value}</button>
-  const [value, setValue] = useState(null);
-
+  /*const [value, setValue] = useState(null);
   function handleClick() {
     setValue('X');
   }
@@ -51,29 +50,30 @@ function Square(){
     >
       {value}
     </button>
-  );
+  );*/
+  return <button className="square">{value}</button>;
 
 }
 
 
 export default function Board() {
-
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return(
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]} />
+        <Square value={squares[2]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} />
+        <Square value={squares[4]} />
+        <Square value={squares[5]} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} />
+        <Square value={squares[7]} />
+        <Square value={squares[8]} />
       </div>
     </>
   );
