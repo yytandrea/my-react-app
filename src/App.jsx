ebc979dd63ea9c4c5d82400a2e135df0a613009c
yytandrea <andrea.y@wustl.edu>
2025-04-02 12:15:10 -0500

    3-new Square function

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,11 @@ export function App() {
     </>
   )
 }
-
-
 //export default App
+
+function Square(){
+  return <button className="square">1</button>
+}
 
 
 export default function Board() {
@@ -44,20 +46,20 @@ export default function Board() {
   return(
     <>
       <div className="board-row">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
-        </div>
-        <div className="board-row">
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
-        </div>
-        <div className="board-row">
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
-        </div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
