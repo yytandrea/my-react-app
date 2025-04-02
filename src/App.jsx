245b28acb116e78b4537505cff10e58e238453cb
yytandrea <andrea.y@wustl.edu>
2025-04-02 13:54:44 -0500

    15- added Game function

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function Square( {value, onSquareClick}){
 }
 
 
-export default function Board() {
+function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
   
@@ -127,4 +127,19 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+
+
+export default function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <ol>{/*TODO*/}</ol>
+      </div>
+    </div>
+  );
 }
