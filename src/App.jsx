9b0166c16beeb3751965472ecae94fa8cd8dd5fe
yytandrea <andrea.y@wustl.edu>
2025-04-02 12:29:43 -0500

    6- X shows up on click

--- src/App.jsx
+++ src/App.jsx
@@ -34,13 +34,14 @@ import './App.css'
     </>
   )
 }*/
-//export default App
 
-function Square({value}){
+
+function Square(){
   //return <button className="square">{value}</button>
+  const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -60,19 +61,19 @@ export default function Board() {
   return(
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
